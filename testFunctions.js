ef9e419c6b071f76404bb2a90b4816c7194fa334
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-02-06 00:36:49 -0700

    Went through and linted the entire project.

--- testFunctions.js
+++ testFunctions.js
@@ -1,91 +1,92 @@
 module.exports.getAllReservation = async function getAllReservation(rp, resevationsUrl) {
-    var options = {
-        uri: `${resevationsUrl}/reservations`,
-         headers:{
-                },
-    };
-    
-    let funcResp = {
-        errorWasCaught:false,
-        errorCaught:null
-    }
-        
-    try{
-        var response = await rp(options);
-     } catch (exception){
-        funcResp.errorCaught=exception;
-        funcResp.errorWasCaught=true;
-    }
-        
-    return funcResp
-}
+  const options = {
+    uri: `${resevationsUrl}/reservations`,
+    headers: {
+    },
+  };
 
-module.exports.makeReservation = async function makeReservation(rp, reservationsUrl){
-    let options = {
-        method: 'POST',
-        uri: `${reservationsUrl}/reservations`,
-        headers: {
+  const funcResp = {
+    errorWasCaught: false,
+    errorCaught: null,
+  };
+
+  try {
+    const response = await rp(options);
+  } catch (exception) {
+    funcResp.errorCaught = exception;
+    funcResp.errorWasCaught = true;
+  }
+
+  return funcResp;
+};
+
+module.exports.makeReservation = async function makeReservation(rp, reservationsUrl) {
+  const options = {
+    method: 'POST',
+    uri: `${reservationsUrl}/reservations`,
+    headers: {
+    },
+    body: {
+      customerId: 64209000,
+      reservationItems: [
+        {
+          description: 'Canoe',
+          itemId: 8000000,
         },
-        body: {
-            "customerId": "64209000",
-            "reservationItems": [
-                {
-                    "description": "Canoe",
-                    "itemId": 8000000
-                },
-                {
-                    "description": "Paddle",
-                    "itemId": 4949491
-                },
-                {
-                    "description": "Paddle",
-                    "itemId": 4949491
-                },
-                {
-                    "description": "Life Jacket",
-                    "itemId": 4949491
-                }
-            ],
-            "dueDate": 1610148694321
+        {
+          description: 'Paddle',
+          itemId: 4949491,
         },
-        json: true,
-        simple: false,
-    };
-
-    let funcResp = {
-        errorWasCaught:false,
-        errorCaught:null,
-        response:null
-    }
-
-    try{
-        funcResp.response = await rp(options);
-     } catch (exception){
-        errorCaught=exception;
-        errorWasCaught=true;
-    }
-    
-    return funcResp;
-}
-
-module.exports.getSingleReservation = async function getSingleReservation(rp, reservationsUrl, create){
-    var options = {
-        uri: `${reservationsUrl}/reservations/${create}`,
-        headers:{
+        {
+          description: 'Paddle',
+          itemId: 4949491,
         },
-    };
+        {
+          description: 'Life Jacket',
+          itemId: 4949491,
+        },
+      ],
+      dueDate: 1610148694321,
+    },
+    json: true,
+    simple: false,
+  };
 
-    let funcResp = {
-        errorWasCaught:false,
-        errorCaught:null
-    }
+  const funcResp = {
+    errorWasCaught: false,
+    errorCaught: null,
+    response: null,
+  };
 
-    try{
-        var response = await rp(options);
-     } catch (exception){
-        funcResp.errorCaught=exception;
-        funcResp.errorWasCaught=true;
-    }
+  try {
+    funcResp.response = await rp(options);
+  } catch (exception) {
+    funcResp.errorCaught = exception;
+    funcResp.errorWasCaught = true;
+  }
 
-    return funcResp;
-}
+  return funcResp;
+};
+
+module.exports.getSingleReservation = async function
+getSingleReservation(rp, reservationsUrl, create) {
+  const options = {
+    uri: `${reservationsUrl}/reservations/${create}`,
+    headers: {
+    },
+  };
+
+  const funcResp = {
+    errorWasCaught: false,
+    errorCaught: null,
+  };
+
+  try {
+    const response = await rp(options);
+  } catch (exception) {
+    funcResp.errorCaught = exception;
+    funcResp.errorWasCaught = true;
+  }
+
+  return funcResp;
+};
--- tests/runAll.test.js
+++ tests/runAll.test.js
@@ -1,22 +1,19 @@
-//jest.mock('../testFunctions.js');
-//const f = jest.requireActual('../testFunctions.js');
-
 const rp = require('request-promise-native');
 const config = require('config');
 const f = require('../testFunctions');
 
 const reservationsUrl = config.get('reservations-url');
-it('Testing to see if ${reservationsUrl} is up', async () =>{
-    //let f = new functions;
-    let response1 = await f.getAllReservation(rp, reservationsUrl);
+it(`Testing to see if ${reservationsUrl} is up`, async () => {
+  const response1 = await f.getAllReservation(rp, reservationsUrl);
 
-    expect(response1.errorWasCaught).toBe(false);//assertion of what is expected
+  expect(response1.errorWasCaught).toBe(false); // assertion of what is expected
 
-    let response2 = await f.makeReservation(rp, reservationsUrl);
+  let response2 = await f.makeReservation(rp, reservationsUrl);
 
-    expect(response2.errorWasCaught).toBe(false);//assertion of what is expected
+  expect(response2.errorWasCaught).toBe(false); // assertion of what is expected
 
-    let response3 = await f.getSingleReservation(rp, reservationsUrl, response2.response.substring(9));
+  const response3 = await f.getSingleReservation(rp,
+    reservationsUrl, response2.response.substring(9));
 
-    expect(response3.errorWasCaught).toBe(false);//assertion of what is expected
-})
+  expect(response3.errorWasCaught).toBe(false); // assertion of what is expected
+});
