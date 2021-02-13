b1032cd785a94f488301dfd331f48cd8dd11566d
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-02-12 20:34:38 -0700

    Linted this branch

--- tests/runAll.test.js
+++ tests/runAll.test.js
@@ -4,35 +4,35 @@ const f = require('../testFunctions');
 
 const reservationsUrl = config.get('reservations-url');
 it(`Testing to see if ${reservationsUrl} is up`, async () => {
-  let respArray = [];
+  const respArray = [];
   const email = `${Date.now()}@test.com`;
 
   // Checks if the page to get all reservations is working.
   const respGetAllRes = await f.getAll(rp, `${reservationsUrl}/reservations`);
-  respArray[respArray.length] = {name: 'Get all reservations', working: !respGetAllRes.errorWasCaught};
-  
+  respArray[respArray.length] = { name: 'Get all reservations', working: !respGetAllRes.errorWasCaught };
+
   // Checks if the page to get all customers is working.
   const respGetAllCust = await f.getAll(rp, `${reservationsUrl}/customer`);
-  respArray[respArray.length] = {name: 'Get all customers', working: !respGetAllCust.errorWasCaught};
+  respArray[respArray.length] = { name: 'Get all customers', working: !respGetAllCust.errorWasCaught };
 
   // Checks if can create a user.
   const respCreateCust = await f.createCustomer(rp, `${reservationsUrl}/customer`, email);
-  respArray[respArray.length] = {name: 'Create customer', working: !respCreateCust.errorWasCaught};
+  respArray[respArray.length] = { name: 'Create customer', working: !respCreateCust.errorWasCaught };
 
   // Checks if can create a reservation.
   const respCreateRes = await f.createReservation(rp, `${reservationsUrl}/reservations`, email);
-  respArray[respArray.length] = {name: 'Create reservation', working: !respCreateRes.errorWasCaught};
+  respArray[respArray.length] = { name: 'Create reservation', working: !respCreateRes.errorWasCaught };
 
   // Checks if can get a customer.
   const respGetSingCust = await f.getSingle(rp, `${reservationsUrl}/customer`, email);
-  respArray[respArray.length] = {name: 'Get one customer', working: !respGetSingCust.errorWasCaught};
-  
+  respArray[respArray.length] = { name: 'Get one customer', working: !respGetSingCust.errorWasCaught };
+
   // Checks if can get a reservation.
   const respGetSingRes = await f.getSingle(rp, `${reservationsUrl}/reservations`, respCreateRes.response);
-  respArray[respArray.length] = {name: 'Get one reservation', working: !respGetSingRes.errorWasCaught};
+  respArray[respArray.length] = { name: 'Get one reservation', working: !respGetSingRes.errorWasCaught };
 
   let str = '';
-  for (let i of respArray) {
+  for (const i of respArray) {
     str += `${i.name} is currently ${i.working ? '' : 'NOT '}working.\n`;
   }
   console.log(str);
