b7552231dd31b1eab4c9cc949d57a08dfd49bd29
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-02-12 20:25:14 -0700

    Updated style through linter

--- tests/createAndGetSingleReservation.test.js
+++ tests/createAndGetSingleReservation.test.js
@@ -1,40 +1,40 @@
 const rp = require('request-promise-native');
 const config = require('config');
 
-const reservationUrl = config.get('reservations-url')+'/reservations';
+const reservationUrl = `${config.get('reservations-url')}/reservations`;
 
 async function createReservation() {
-  let options = {
+  const options = {
     method: 'POST',
     uri: reservationUrl,
     headers: {
     },
     body: {
-      "customerId": "sam.test@test.com",
-      "reservationItems": [
+      customerId: 'sam.test@test.com',
+      reservationItems: [
         {
-          "description": "Canoe",
-          "itemId": 8000000
+          description: 'Canoe',
+          itemId: 8000000,
         },
         {
-          "description": "Paddle",
-          "itemId": 4949491
+          description: 'Paddle',
+          itemId: 4949491,
         },
         {
-          "description": "Paddle",
-          "itemId": 4949491
+          description: 'Paddle',
+          itemId: 4949491,
         },
         {
-          "description": "Life Jacket",
-          "itemId": 4949491
-        }
+          description: 'Life Jacket',
+          itemId: 4949491,
+        },
       ],
-      "dueDate": 1610148694321
+      dueDate: 1610148694321,
     },
     json: true,
     simple: false,
   };
-  
+
   // Creates object that holds any errors and the api response.
   const funcResp = {
     errorWasCaught: false,
@@ -45,28 +45,23 @@ async function createReservation() {
   // Standard try/catch to handle any errors.
   try {
     funcResp.response = await rp(options);
-    funcResp.response = funcResp.response.substr(9); // Cuts off beginning of message, leaving just the reservation id.
+    // Cuts off beginning of message, leaving just the reservation id.
+    funcResp.response = funcResp.response.substr(9);
   } catch (exception) {
     funcResp.errorCaught = exception;
     funcResp.errorWasCaught = true;
   }
-  
-  return funcResp;
-}
 
-async function getSingleReservation() {
-  const options = {
-    url: reservationUrl + "/" + reservationId,
-  };
+  return funcResp;
 }
 
 it(`Testing to see if ${reservationUrl} is up`, async () =>{
   // Calls create response, which return an object with errors and the reservation id.
   const createResponse = await createReservation();
 
-  var options = {
+  const options = {
     uri: `${reservationUrl}/${createResponse.response}`,
-    headers:{
+    headers: {
     },
   };
 
@@ -74,21 +69,21 @@ it(`Testing to see if ${reservationUrl} is up`, async () =>{
   const funcResp = {
     errorWasCaught: false,
     errorCaught: null,
-  }
+  };
 
   // Standard try/catch to keep from crashing if site isn't up.
-  try{
-    var response = await rp(options);
-  } catch (exception){
-    funcResp.errorCaught=exception;
-    funcResp.errorWasCaught=true;
+  try {
+    const response = await rp(options);
+  } catch (exception) {
+    funcResp.errorCaught = exception;
+    funcResp.errorWasCaught = true;
   }
 
   // Outputs a string that shows which of the tests are failing.
-  let str = `Create reservation is currently ${createResponse.errorWasCaught ? 'NOT ' : ''}working.\n` 
-          + `Get single reservation is currently ${funcResp.errorWasCaught ? 'NOT ' : ''}working`;
+  const str = `Create reservation is currently ${createResponse.errorWasCaught ? 'NOT ' : ''}working.\n`
+            + `Get single reservation is currently ${funcResp.errorWasCaught ? 'NOT ' : ''}working`;
 
   console.log(str);
-  expect(createResponse.errorWasCaught).toBe(false);//assertion of what is expected
-  expect(funcResp.errorWasCaught).toBe(false);//assertion of what is expected
-})
+  expect(createResponse.errorWasCaught).toBe(false);// assertion of what is expected
+  expect(funcResp.errorWasCaught).toBe(false);// assertion of what is expected
+});
--- tests/reservations.test.js
+++ tests/reservations.test.js
@@ -1,26 +1,23 @@
 const rp = require('request-promise-native');
 const config = require('config');
 
-const reservationsUrl = config.get('reservations-url')+'/reservations';
-it('Testing to see if ${reservationsUrl} is up', async () =>{
-    
-    var options = {
-        uri: reservationsUrl,
-        // Reservation
-        headers:{
-        },
-    };
+const reservationsUrl = `${config.get('reservations-url')}/reservations`;
+it(`Testing to see if ${reservationsUrl} is up`, async () => {
+  const options = {
+    uri: reservationsUrl,
+    headers: {
+    },
+  };
 
-    var errorWasCaught=false;
-    var errorCaught=null;
+  let errorWasCaught = false;
+  let errorCaught = null;
 
-    try{
-        var response = await rp(options);
-        //console.log(response)
-    } catch (exception){
-        errorCaught=exception;
-        errorWasCaught=true;
-    }
-    expect(errorWasCaught).toBe(false);//assertion of what is expected
-})
-
+  try {
+    const response = await rp(options);
+    // console.log(response);
+  } catch (exception) {
+    errorCaught = exception;
+    errorWasCaught = true;
+  }
+  expect(errorWasCaught).toBe(false);// assertion of what is expected
+});
